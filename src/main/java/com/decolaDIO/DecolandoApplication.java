2c71eea186fbd93b399424a526b8800b06427061
BBRRUUNNOO <brunoricardo050@gmail.com>
2025-03-24 17:05:50 -0300

    Initial commit

---
+++ src/main/java/com/decolaDIO/DecolandoApplication.java
@@ -0,0 +1,13 @@
+package com.decolaDIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decolaDIO/DecolandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decolaDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
